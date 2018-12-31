4f989f6207d0c7a8dabd1fefab15f89d8a2ede7a
Nijamudeen Mohamed <nijamudeensulaiman@syncfusion.com>
2018-12-31 17:59:36 +0530

    Getting started sample added
    Getting started sample added

---
+++ GettingStartedSchedule/ScheduleSimpleSample/ScheduleSimpleSample/ScheduleSimpleSamplePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ScheduleSimpleSample
+{
+    public partial class ScheduleSimpleSamplePage : ContentPage
+    {
+        public ScheduleSimpleSamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
